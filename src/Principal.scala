532d192af44d1cc86e55c4221f85cfb8ac579a79
AlanOGxD <alanosvaldo88@gmail.com>
2021-03-19 18:30:51 -0600

    Se creó clase paciente

--- src/Principal.scala
+++ src/Principal.scala
@@ -1,6 +1,6 @@
-
-
-object Principal {
+class Paciente(nombre:String, primerAp:String,segundoAP:String, edad:Byte,
+               val fecha:Array[String], val horaDeRegistro:Array[String],val nivelBienestar:Array[Byte], 
+               val temperaturas:Array[Double],val humedad:Array[Double]) {
   
   def nivelPromBien(): Unit ={
     
@@ -10,9 +10,17 @@ object Principal {
   
   def tempMenor(): Unit={}
   
+}
+
+object Principal {
+  
+  
+  
   
   def main(args: Array[String]): Unit = {
-    val p = new Paciente();
+    
+    
+    //val p = new Paciente
     
   }
 }
